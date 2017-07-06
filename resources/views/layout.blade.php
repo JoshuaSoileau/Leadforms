97dd497364b5197cef527c5e0aff5ace3ea328e7
Josh Soileau <joshua.soileau@gmail.com>
2017-07-06 08:44:24 -0400

    More things

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,16 +8,7 @@
             @include('header')
         </div>
         <div class="row">
-            <div class="col-2">
-                <nav class="nav-sidebar">
-                    <ul class="nav">
-                        <li class="active"><a href="javascript:;">All Forms</a></li>
-                        <li><a href="javascript:;">All Leads</a></li>
-                        <li><a href="javascript:;">My Settings</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="col-10">
+            <div class="container">
                 @yield('content')
             </div>
         </div>
